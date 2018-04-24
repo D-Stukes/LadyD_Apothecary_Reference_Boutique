671e835dc65affd50c812ebdfaccd089881f11b3
student_23 <student_23@ool-ad0200e6.dyn.optonline.net>
2018-04-24 10:16:18 -0400

    more styling

--- ladyd_boutique/db/seeds.sql
+++ ladyd_boutique/db/seeds.sql
@@ -22,18 +22,18 @@ VALUES
 INSERT INTO products
   (product_name, description, price, category_id, image_url)
 VALUES
-  ('Pine-Tar bar', 'antseptic cleanser', 5.00,  1, './public/images/herbal-bouquet.jpg'),
-  ('Lavender bar', 'fragrant bar', 7.00, 1, './public/images/herbal-bouquet.jpg'),
-  ('Olive Oil', 'Cardiovascular support', 15.00, 2, './public/images/herbal-bouquet.jpg'),
-  ('Broccoli Rabe','Eliminates nausea', 4.00, 3, './public/images/herbal-bouquet.jpg'),
-  ('Tea Tree Oil', 'Posesses antifungul and antiseptic properties', 9.00, 2, './public/images/herbal-bouquet.jpg'),
-  ('Ginger', 'Increases circulation', 6.50, 3, './public/images/herbal-bouquet.jpg'),
-  ('Chamomile bar', 'Calming aromatherapy', 5.00, 1, './public/images/herbal-bouquet.jpg'),
-  ('Shea Butter bar', 'Moisturizes and conditions skin', 3.00, 1, './public/images/herbal-bouquet.jpg'),
-  ('Cayenne Pepper', 'Increases metabolism', 2.00,  3, './public/images/herbal-bouquet.jpg'),
-  ('Clove Oil', 'Antiseptic and aromatic', 11.99, 2, './public/images/herbal-bouquet.jpg'),
-  ('Raw Cacao Powder','Provides excellent circulation support', 18.00, 3, './public/images/herbal-bouquet.jpg'),
-  ('Maca Powder', 'Balances hormones', 13.00, 3, './public/images/herbal-bouquet.jpg'),
-  ('Raw vitamin C', 'Supplies the body with antioxidants and fights cellular rusting', 25.00, 4, './public/images/herbal-bouquet.jpg'),
-  ('Probiotics', 'Restores good bacteria in gut', 30.00, 4, './public/images/herbal-bouquet.jpg'),
-  ('Ubiquinol', 'Supports a healthy heart', 15.00, 4, './public/images/herbal-bouquet.jpg');
+  ('Pine-Tar bar', 'antseptic cleanser', 5.00,  1, 'images/herbal-bouquet.jpg'),
+  ('Lavender bar', 'fragrant bar', 7.00, 1, 'images/herbal-bouquet.jpg'),
+  ('Olive Oil', 'Cardiovascular support', 15.00, 2, 'images/herbal-bouquet.jpg'),
+  ('Broccoli Rabe','Eliminates nausea', 4.00, 3, 'images/giant-dandelion.jpg'),
+  ('Tea Tree Oil', 'Posesses antifungul and antiseptic properties', 9.00, 2, 'images/herbal-bouquet.jpg'),
+  ('Ginger', 'Increases circulation', 6.50, 3, 'images/herbal-bouquet.jpg'),
+  ('Chamomile bar', 'Calming aromatherapy', 5.00, 1, 'images/herbal-bouquet.jpg'),
+  ('Shea Butter bar', 'Moisturizes and conditions skin', 3.00, 1, 'images/herbal-bouquet.jpg'),
+  ('Cayenne Pepper', 'Increases metabolism', 2.00,  3, 'images/herbal-bouquet.jpg'),
+  ('Clove Oil', 'Antiseptic and aromatic', 11.99, 2, 'images/herbal-bouquet.jpg'),
+  ('Raw Cacao Powder','Provides excellent circulation support', 18.00, 3, 'images/herbal-bouquet.jpg'),
+  ('Maca Powder', 'Balances hormones', 13.00, 3, '/images/herbal-bouquet.jpg'),
+  ('Raw vitamin C', 'Supplies the body with antioxidants and fights cellular rusting', 25.00, 4, '/images/herbal-bouquet.jpg'),
+  ('Probiotics', 'Restores good bacteria in gut', 30.00, 4, '/images/healingfoods.jpg'),
+  ('Ubiquinol', 'Supports a healthy heart', 15.00, 4, '/images/herbal-bouquet.jpg');
